c76ca1252cb71bee7eb2f41c7b5e642529ca4f2f
SyahrulMbrk <syahrulmbrk16@upi.edu>
2025-03-18 22:32:03 +0700

    ObjekdanKelasGenap.java

--- ObjekdanKelasGenap.java
+++ ObjekdanKelasGenap.java
@@ -5,19 +5,16 @@ public class ObjekdanKelasGenap {
     public static class PersegiPanjang { 
         private int panjang, lebar;
 
-        // Konstruktor default
         public PersegiPanjang() {
             this.panjang = 0;
             this.lebar = 0;
         }
 
-        // Konstruktor dengan parameter
         public PersegiPanjang(int panjangBaru, int lebarBaru) {
             this.panjang = panjangBaru;
             this.lebar = lebarBaru;
         }
 
-        // Getter dan Setter
         public int getPanjang() {
             return panjang;
         }
@@ -34,24 +31,20 @@ public class ObjekdanKelasGenap {
             this.lebar = lebar;
         }
 
-        // Metode untuk menghitung luas
         public int getLuas() {
             return panjang * lebar;
         }
 
-        // Metode untuk menghitung keliling
         public int getKeliling() {
             return 2 * (panjang + lebar);
         }
     }
 
     public static void main(String[] args) {
-        // Membuat objek PersegiPanjang
         PersegiPanjang perpan1 = new PersegiPanjang(1, 1);
         PersegiPanjang perpan2 = new PersegiPanjang(30, 40);
         PersegiPanjang perpan3 = new PersegiPanjang(25, 35);
         
-        // Menampilkan hasil
         tampilkanDetail(perpan1, "Persegi Panjang 1");
         tampilkanDetail(perpan2, "Persegi Panjang 2");
         tampilkanDetail(perpan3, "Persegi Panjang 3");
